9ec2bcba6152c29b7f8bba61b328e420268dde11
Dan Sdeor <dansdeor@campus.technion.ac.il>
2022-12-12 14:10:31 +0200

    Simple Hello

--- HW2/Wet/linux-4.15.18-custom/init/main.c
+++ HW2/Wet/linux-4.15.18-custom/init/main.c
@@ -949,6 +949,7 @@ void __init load_default_modules(void)
 
 static int run_init_process(const char *init_filename)
 {
+	printk("Hello, Kernel!\n");
 	argv_init[0] = init_filename;
 	return do_execve(getname_kernel(init_filename),
 		(const char __user *const __user *)argv_init,
